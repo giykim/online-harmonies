ac133d62f61bb1d85362728bd47ddaa00046b19b
Giyoung Kim <gk122@duke.edu>
2026-03-29 17:59:21 -0500

    Create TokenColor enum

---
+++ Sources/OnlineHarmonies/Models/TokenColor.swift
@@ -0,0 +1,15 @@
+//
+//  TokenColor.swift
+//  OnlineHarmonies
+//
+//  Created by Giyoung Kim on 3/29/26.
+//
+
+enum TokenColor {
+    case blue
+    case gray
+    case brown
+    case green
+    case yellow
+    case red
+}
